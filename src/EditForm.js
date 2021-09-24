4f82616e4ec1e415f2267f647f6d2bc46ca9dbc1
Vasily93 <markovavasily@gmail.com>
2021-09-23 22:26:27 -0700

    couple style twicks

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import {Paper, TextField} from '@material-ui/core';
+import {TextField} from '@material-ui/core';
 import useInputState from './hooks/useInputState';
 
 function EditForm(props) {
     const [value, handleChange] = useInputState(props.task)
     return(
-        <Paper style={{margin: '1rem 0', padding: '0 1rem'}}>
             <form onSubmit={(e) => {
                 e.preventDefault()
                 props.editTodo(value, props.id)
                 props.toggle()
-            }}>
+                }}
+                style={{margin: '1rem 0', width: '50%'}}
+            >
                 <TextField value={value}
                 onChange={handleChange}
                 margin='normal' 
                 label='Editing'
                 fullWidth
+                autoFocus
                 />
             </form>
-        </Paper>
     )
 }
 
